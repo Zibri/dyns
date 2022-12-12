87be7157f3b9cffb2c9ea27ff1dae6448d133d7d
Zibri <Zibri@users.noreply.github.com>
2022-12-12 10:21:08 +0200

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,99 @@
+function requestDeviceMotion(callback) {
+  if (window.DeviceMotionEvent && DeviceMotionEvent.requestPermission) {
+    DeviceMotionEvent.requestPermission().then(
+      function (state) {
+        if (state == "granted") {
+          callback(null);
+        } else callback(new Error("Permission denied by user"));
+      },
+      function (err) {
+        callback(err);
+      }
+    );
+  } else if (window.ondevicemotion !== undefined) {
+    callback(null);
+  } else callback(new Error("DeviceMotion is not supported."));
+}
+//
+var canvas = document.getElementById("dm_graphs");
+var ctx = canvas.getContext("2d");
+//            ctx.fillStyle = "#000000";
+//            ctx.fillRect(0, 0, canvas.width, canvas.height);
+var graphX = 791;
+function updateFancyGraphs(e) {
+  var rot = e.rotationRate;
+	var acc = e.acceleration || e.accelerationIncludingGravity;
+
+  var gh = canvas.height;
+  var gh2 = gh/2;
+  function drawGraph(val, color) {
+    if (val == null) val = 0;
+    var size = Math.max(-gh, Math.min(val * 10, gh));
+    ctx.fillStyle = color;
+    ctx.fillRect(graphX,  gh2, 1, size/2);
+  }
+
+  ctx.drawImage(canvas, -1, 0);
+  ctx.fillStyle = "black";
+//  ctx.fillRect(graphX, 0, 2, canvas.height);
+  ctx.fillRect(graphX, 0, 2, canvas.height);
+
+  var a=rot.alpha,b=rot.beta,c=rot.gamma;
+  drawGraph(3*(a+b+c), "blue"); 
+  
+//  drawGraph(acc.z*2, 3, "blue");
+//  drawGraph(acc.x*2, 4, "lime");
+//  drawGraph(acc.y*2, 5, "aqua");
+//  graphX = (graphX + 1) % canvas.width;
+}
+//
+function resizeCanvas() {
+  var status = document.getElementById("dm_status");
+status.innerText = window.innerWidth+"x"+window.innerHeight;
+  var w = window.innerWidth || document.body.offsetWidth;
+//  canvas.width = w;
+//  canvas.height=window.innerHeight;
+}
+window.addEventListener("resize", resizeCanvas);
+window.addEventListener("deviceorientation", resizeCanvas);
+resizeCanvas();
+//
+function gofs(e) {
+  document.getElementById("fs").requestFullscreen()
+  document.getElementById("dm_graphs").height=400;
+const requestWakeLock = async () => {
+  try {
+
+    const wakeLock = await navigator.wakeLock.request('screen');
+
+  } catch (err) {
+    // The wake lock request fails - usually system-related, such as low battery.
+alert('wakelock not acquired');
+    console.log(`${err.name}, ${err.message}`);
+  }
+}
+
+requestWakeLock();
+  setTimeout(()=>
+  window.screen.orientation.lock("landscape"),50)
+}
+
+function firstClick(e) {
+  var status = document.getElementById("dm_status");
+  if (e) status.innerText = "Requesting...";
+  requestDeviceMotion(function (err) {
+    if (!err) {
+      status.innerText = "OK!";
+      window.removeEventListener("click", firstClick);
+      window.removeEventListener("touchend", firstClick);
+      window.addEventListener("devicemotion", updateFancyGraphs);
+      el=document.querySelector('#fs');
+      el.addEventListener("click", gofs);
+    } else if (e) {
+      status.innerText = "" + err;
+    }
+  }, e);
+}
+window.addEventListener("click", firstClick);
+window.addEventListener("touchend", firstClick);
+firstClick(false);
