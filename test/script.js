466ea20edacbc1146f4630e63f09a0ecdb99deba
Zibri <Zibri@users.noreply.github.com>
2022-12-14 11:24:45 +0200

    Update script.js

--- test/script.js
+++ test/script.js
@@ -19,7 +19,7 @@ var canvas = document.getElementById("dm_graphs");
 var ctx = canvas.getContext("2d");
 //            ctx.fillStyle = "#000000";
 //            ctx.fillRect(0, 0, canvas.width, canvas.height);
-var graphX = window.innerWidth-1;
+//var graphX = window.innerWidth-1;
 function updateFancyGraphs(e) {
 
   var rot = e.rotationRate;
@@ -30,14 +30,14 @@ function updateFancyGraphs(e) {
   function drawGraph(val, color) {
     if (val == null) val = 0;
     var size = Math.max(-gh, Math.min(val * 10, gh));
-    ctx.fillStyle = color;
-    ctx.fillRect(graphX,  gh2, 1, size/2);
+    ctx.strokeStyle = color;
+    ctx.strokeRect(graphX,  gh2, 1, size/2);
   }
 
   ctx.drawImage(canvas, -1, 0);
-  ctx.fillStyle = "black";
+  ctx.strokeStyle = "black";
 //  ctx.fillRect(graphX, 0, 2, canvas.height);
-  ctx.fillRect(graphX, 0, 2, canvas.height);
+  ctx.strokeRect(graphX, 0, 2, canvas.height);
 
   var a=rot.alpha,b=rot.beta,c=rot.gamma;
   drawGraph(3*(a+b+c), "blue"); 
@@ -50,23 +50,25 @@ function updateFancyGraphs(e) {
 //
 function resizeCanvas() {
   var status = document.getElementById("dm_status");
-status.innerText = window.innerWidth+"x"+window.innerHeight;
+  status.innerText = window.innerWidth + "x" + window.innerHeight;
+  status.innerText += "  " + canvas.width + "x" + canvas.height;
   var w = window.innerWidth || document.body.offsetWidth;
-//  canvas.width = w;
-//  canvas.height=window.innerHeight;
+  canvas.width = w / 1.5;
+  canvas.height = window.innerHeight / 1.5;
+  graphX = canvas.width - 1;
 }
 window.addEventListener("resize", resizeCanvas);
-window.addEventListener("deviceorientation", resizeCanvas);
-resizeCanvas();
+//window.addEventListener("deviceorientation", resizeCanvas);
+//resizeCanvas();
 //
 function gofs(e) {
-  document.getElementById("fs").requestFullscreen()
-  document.getElementById("dm_graphs").height=400;
+  canvas.requestFullscreen()
+
 const requestWakeLock = async () => {
   try {
 
     const wakeLock = await navigator.wakeLock.request('screen');
-    screen.orientation.lock('landscape');
+    setTimeout(()=>window.screen.orientation.lock("landscape"),50)
 
   } catch (err) {
     // The wake lock request fails - usually system-related, such as low battery.
@@ -75,8 +77,7 @@ const requestWakeLock = async () => {
 }
 
 requestWakeLock();
-  setTimeout(()=>
-  window.screen.orientation.lock("landscape"),50)
+  
 }
 
 function firstClick(e) {
@@ -96,7 +97,7 @@ function firstClick(e) {
   }, e);
 }
 window.addEventListener("click", firstClick);
-window.addEventListener("touchend", firstClick);
+//window.addEventListener("touchend", firstClick);
 //firstClick(false);
 ctx.font = "30px Georgia";
 ctx.fillStyle = 'yellow';
