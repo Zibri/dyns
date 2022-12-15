d5a6f60fff26111db09ed456e8f9825d5ce7d081
Zibri <Zibri@users.noreply.github.com>
2022-12-15 10:49:09 +0200

    Update script.js

--- test/script.js
+++ test/script.js
@@ -84,6 +84,8 @@ function firstClick(e) {
 			window.addEventListener("devicemotion", updateFancyGraphs);
 			el = document.querySelector('#fs');
 			el.addEventListener("click", gofs);
+navigator.userAgentData.mobile &&
+z=setInterval(()=>updateFancyGraphs({ "rotationRate": { "alpha": 7.5-Math.random()*15, "beta": 7.5-Math.random()*15, "gamma": 7.5-Math.random()*15}}),20)
 		} else if (e) {
 			status.innerText = "" + err;
 		}
